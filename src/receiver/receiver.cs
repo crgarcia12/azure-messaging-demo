8dc0f7aef6e518a9612b8cf771369a39c100ad54
AutoExtractor <crgarcia@live.com.ar>
2021-06-01 00:08:57 +0200

    prepare for kusto

--- src/receiver/receiver.cs
+++ src/receiver/receiver.cs
@@ -10,7 +10,7 @@ namespace receiver
         [FunctionName("receiver")]
         public static void Run([ServiceBusTrigger("messagesqueue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
         {   
-            log.LogInformation($"QueueMessage;{myQueueItem}{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+            log.LogInformation($"[QueueMessage];{myQueueItem};{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
         }
     }
 }
