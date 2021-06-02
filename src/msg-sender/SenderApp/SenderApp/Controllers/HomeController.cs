2854ab096fa79f0e44a4ef32b435c29dac46b21d
AutoExtractor <crgarcia@live.com.ar>
2021-06-02 10:29:22 +0200

    cmt

--- src/msg-sender/SenderApp/SenderApp/Controllers/HomeController.cs
+++ src/msg-sender/SenderApp/SenderApp/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace SenderApp.Controllers
 {
+    //Comment
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
